b755f227e3155154238470547cbb2673213876ca
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-09-05 15:54:38 -0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const calculus = document.querySelector('.calculate');
 calculus.addEventListener('click', () => {
     document.getElementById('results').innerHTML =
-    `
+        `
     <div id="new-results">
     <h2>Your results</h2>
     <p>Your results are shown below based on the information<br>you provided. To adjust the results, edit the form and<br> click "calculate repayments" again.</p>
@@ -13,20 +13,22 @@ calculus.addEventListener('click', () => {
     </div>
     </div>
     `
-    
+    const isInterestOnly = document.getElementById('interest-only').checked;
+    if(isInterestOnly) {
         interest();
-    
+    } else {
         calculateMortgage();
-    
-    
+    }
 });
 
+//adiciona listeners aos radio buttons
 const interestOnly = document.getElementById('interest-only');
 interestOnly.addEventListener('change', interest);
 
 const mortgage = document.getElementById('repayment1');
 mortgage.addEventListener('change', calculateMortgage);
 
+//função para calcular interest only
 function interest() {
     const mortgageAmount = parseFloat(document.getElementById('amount').value);
     const interestRate = parseFloat(document.getElementById('rate').value);
@@ -36,6 +38,7 @@ function interest() {
 
 };
 
+//função para calcular hipoteca
 function calculateMortgage() {
     const principal = parseFloat(document.getElementById('amount').value);
     const annualInterestRate = parseFloat(document.getElementById('rate').value);
